3a101cb707e6271099bca81c5bade0d0e329c2ae
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-09-29 18:17:08 +0300

    structure

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import foodList from './menu.json';
+
+
+const data = foodList[0];
+
+function FoodCard(props) {
+  return (
+    <div style={{ width: "480px" }}>
+      <img src={data.image} alt={data.name} width="480px" />
+      <div>
+        <h2>{data.name}</h2>
+        <p>{data.price} кредитов</p>
+        <p>{data.description}</p>
+      </div>
+    </div>
+  );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FoodCard />);
+
+
+
