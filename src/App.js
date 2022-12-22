c77938070600559f610d9b8acdaea6a84e2b4a2b
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-12-23 01:10:39 +0200

    rewritten on hooks

--- src/App.js
+++ src/App.js
@@ -1,58 +1,42 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { Component } from "react";
 import s from "./App.module.css";
-import Statistics from "./components/Statistics/Statistics";
-import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
 
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const onButtonClick = (e) => {
-    switch (e.target.name) {
-      case 'Good':
-        setGood(good + 1);
-        break;
-      case 'Neutral':
-        setNeutral(neutral + 1);
-        break;
-      case 'Bad':
-        setBad(bad + 1);
-        break;
-      default:
-        return;
-    }
+class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
   }
 
-  const options = ['Good', 'Neutral', 'Bad'];
-
-  const countTotalFeedback = () => {
-    return good + neutral + bad;
+  handleChange = (e) => {
+    this.setState({name: e.target.value})
   }
 
-  const countPositiveFeedback = () => {
-    const total = good + neutral + bad;
-    return Math.round(good / total * 100);
+  handleSubmit = (e) => {
+    e.preventDefault();
   }
 
-  return (
-    <div className={s.container}>
-      <h2>Please leave a feedback</h2>
-      <FeedbackOptions
-        options={options}
-        onLeaveFeedback={onButtonClick}
-      />
-      <h2>Statistics</h2>
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        total={countTotalFeedback}
-        positive={countPositiveFeedback}
-      />
-    </div>
-  )
+  render() {
+    return (
+      <div className={s.container}>
+        <form autoComplete="off">
+          <label className={s.label}>
+            Name
+            <input
+              className={s.input}
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+          <button className={s.button} type="submit">Add contact</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
