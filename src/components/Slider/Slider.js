bd47563c957f49cec662dd6f0f28f24707d414ef
Ivan Amoshyi <amoshyiivan@gmail.com>
2023-01-20 12:41:08 +0200

    slider set up

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,41 +1,43 @@
-import React, { useEffect } from 'react';
-import Swiper, { Navigation, Pagination } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import "./Slider.css";
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import '../../App.css';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper';
 
 export default function Slider() {
-
-  const swiper = new Swiper('.swiper', {
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    speed: 400,
-    spaceBetween: 100,
-    effect: 'slides',
-  });
-
   return (
-    <div className="swiper">
-      <div className="swiper-wrapper">
-        <div className="swiper-slide">Slide 1</div>
-        <div className="swiper-slide">Slide 2</div>
-        <div className="swiper-slide">Slide 3</div>
-      </div>
-      <div className="swiper-pagination"></div>
-      <div className="swiper-button-prev"></div>
-      <div
-        className="swiper-button-next"
-      ></div>
-      <div className="swiper-scrollbar"></div>
-    </div>
+    <main className='SliderApp'>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        slidesPerGroup={1}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </main>
   );
 }
