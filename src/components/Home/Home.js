eb045fc509e08a2fa1f100ddb17bedbb545f86d1
Ivan Amoshyi <amoshyiivan@gmail.com>
2023-02-15 19:56:17 +0200

    dark mode, needs fixes.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,40 @@
-import s from './Home.module.css';
 import { Button } from '@mui/material';
 import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import { useState, useEffect } from 'react';
+import './Home.css';
 
 
 export const Home = () => {
   const location = useLocation();
+  const [mode, setMode] = useState('light');
+
+  useEffect(() => {
+    window.localStorage.setItem('theme', JSON.stringify(mode));
+  }, [mode])
+
+
+  const toggleColorMode = () => {
+    setMode(prevMode =>
+      prevMode === 'light' ? 'dark' : 'light',
+    );
+  };
+
+  const theme = createTheme({
+    palette: {
+      mode: JSON.parse(window.localStorage.getItem('theme')) ?? 'light'
+    },
+  });
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <header className="homeContainer">
         <NavLink
-          className={s.link}
+          className="link"
           to={location.pathname.includes('counter') ? '/' : 'counter'}
         >
           <Button sx={{ margin: '20px' }} variant="contained">
@@ -17,7 +42,7 @@ export const Home = () => {
           </Button>
         </NavLink>
         <NavLink
-          className={s.link}
+          className="link"
           to={location.pathname.includes('search') ? '/' : 'search'}
         >
           <Button sx={{ margin: '20px' }} variant="contained">
@@ -25,16 +50,21 @@ export const Home = () => {
           </Button>
         </NavLink>
         <NavLink
-          className={s.link}
+          className="link"
           to={location.pathname.includes('slider') ? '/' : 'slider'}
         >
           <Button sx={{ margin: '20px' }} variant="contained">
             Slider
           </Button>
         </NavLink>
-        <hr />
+        <FormControlLabel
+          sx={{ marginRight: '20px', marginLeft: 'auto' }}
+          control={<Switch checked={mode === 'dark'} onChange={toggleColorMode} />}
+          label="Dark theme"
+        />
       </header>
+      <hr />
       <Outlet />
-    </>
+    </ThemeProvider>
   );
 };
